c6e44e7d7ddf9f6810ae55625e43c1e62288082f
Wilfred Hughes <me@wilfred.me.uk>
2017-01-29 15:52:45 +0000

    simpler

--- rust_src/src/lib.rs
+++ rust_src/src/lib.rs
@@ -1,9 +1,7 @@
 extern crate libc;
 
 #[repr(C)]
-#[derive(PartialEq, Eq, Clone, Copy)]
 pub struct LispObject(libc::c_int);
-
 
 #[no_mangle]
 pub extern "C" fn Fcdr(list: LispObject) -> LispObject {
